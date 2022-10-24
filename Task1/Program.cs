ab665697dbf428c3363963176087e05efc07a5cb
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-24 23:26:39 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Х координату точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y координату точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double square = Rectangle.GetSquare(r);
+            double perimeter = Rectangle.GetPerimeter(r);
+            double point = Rectangle.GetPoint(r, x, y);
+            Console.WriteLine($"Площадь круга = {square} \nПериметр круга = {perimeter} \nРасстояние от точки до центра окружности = {point}");
+            if (point > r)
+            {
+                Console.WriteLine("Точка не лежит в окружности");
+            }
+            else
+            {
+                Console.WriteLine("Точка лежит в окружности");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Rectangle
+    {
+        static public double GetSquare(double r)
+        {
+            return Math.PI*(r*r);
+        }
+        static public double GetPerimeter(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetPoint(double r, double x, double y)
+        {
+            return Math.Sqrt(((x * x) + (y * y)));
+        }
+    }
+}
